c74573a2ead5a68a695d7e9498f8a2a217786c48
derickard <derickard@gmail.com>
2020-06-20 18:59:13 -0400

    Add project files.

---
+++ StringPermutations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] atoms = new string[] { "ant", "bat", "cow", "cow" };
+            StringPerm p = null;
+            Console.WriteLine("All permutations are:\n");
+            for (int k = 0; (ulong)k < StringPerm.FactorialLookup(4); ++k)
+            {
+                p = new StringPerm(atoms, k);
+                Console.WriteLine(k + " " + p.ToString());
+            }
+        }
+    }
+}
